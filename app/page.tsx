22e4710ff2799433ae8883b7402deeef3deb9d21
isaiasjr.glemao <isaiasjr.glemao@gmail.com>
2024-04-18 16:00:47 +0800

    added vercelignore

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent } from "@/components/ui/card"; 
+import { Card, CardContent } from "@/components/ui/card";
 import { simpleBlogCard } from "./lib/interface";
 import { client, urlFor } from "./lib/sanity";
 import Image from "next/image";
@@ -17,7 +17,7 @@ async function getData() {
   const data = await client.fetch(query);
 
   return data;
-};
+}
 
 export default async function Home() {
   const data: simpleBlogCard[] = await getData();
@@ -26,28 +26,28 @@ export default async function Home() {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
-      {data.map((post) => (
-        <Card>
-          <Image 
+      {data.map((post, index) => (
+        <Card key={index}>
+          <Image
             src={urlFor(post.titleImage).url()}
             alt="image"
             width={500}
-            height={500} 
+            height={500}
             className="rounded-t-lg h-[200px] object-cover"
-            />
+          />
 
-            <CardContent className="mt-5">
-              <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
-              <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">{post.smallDescription}
-              </p>
+          <CardContent className="mt-5">
+            <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
+            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
+              {post.smallDescription}
+            </p>
 
-              <Button asChild className="w-full mt-7">
-                <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
-
-              </Button>
-            </CardContent>
+            <Button asChild className="w-full mt-7">
+              <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+            </Button>
+          </CardContent>
         </Card>
       ))}
     </div>
-  );  
+  );
 }
